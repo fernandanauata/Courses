c7690503538900b3fb757f04f46fe76a28b2f490
Fernanda Nauata <ferhcard@gmail.com>
2016-11-06 00:43:48 -0700

    Cat Clicker Premium

--- frontend-nanodegree/09-Project-Neighborhood-Map/Cat-Clicker-Premium/js/script.js
+++ frontend-nanodegree/09-Project-Neighborhood-Map/Cat-Clicker-Premium/js/script.js
@@ -1,6 +1,19 @@
-var container = '<nav id="#" class="container"></nav>';
-var HTMLimg = '<img class="img-responsive" alt="#" src="%data%">';
-var HTMLcounter = '<h2>%data%: <span id="counter">0</span></h2>';
+var HTMLlist = '<li class="#" onclick="%function%">%data%</li>';
+
+function visible(id){
+    $(".catcontainer").hide();
+    $("#" + id).show();
+};
+
+function counter(id){
+    var counterStr = $('#' + id + "_counter").html();
+    var counter = parseInt(counterStr) + 1;
+    $('#' + id + "_counter").text(counter.toString());
+}
+
+var HTMLcontainer = '<nav id="#" class="catcontainer"></nav>';
+var HTMLimg = '<img class="img-responsive" alt="#" onclick="%function%" src="%data%">';
+var HTMLcounter = '<h2>%data%: <span id="%id%" class="counter">0</span></h2>';
 
 var cat = {
   "catsattr": [{
@@ -33,38 +46,16 @@ var cat = {
 };
 
 cat.display = function() {
-  var test = cat.catsattr.length;
   if (cat.catsattr.length > 0) {
 
       cat.catsattr.forEach(function(cat){
-        $(".cat").append(container.replace("#", cat.id));
-        $(".container:last-child").append(HTMLimg.replace("#", cat.alt).replace("%data%", cat.src));
-        $(".container:last-child").append(HTMLcounter.replace("%data%", cat.title));
+        $(".catlist").append(HTMLlist.replace("#", cat.id).replace("%data%", cat.title).replace("%function%", "visible('" + cat.id +"')"));
+        $(".cat").append(HTMLcontainer.replace("#", cat.id));
+        $(".catcontainer:last-child").append(HTMLimg.replace("#", cat.alt).replace("%data%", cat.src).replace("%function%", "counter('" + cat.id + "')"));
+        $(".catcontainer:last-child").append(HTMLcounter.replace("%data%", cat.title).replace("%id%", cat.id + "_counter"));
       });
-
-  };
+      $(".catcontainer").hide();
+  }
 };
 
 cat.display();
-
-// //Count the clicks for the first cats
-// $('.cat').click(function(e) {
-//   var counterStr = $('#counter').html();
-//   var counter = parseInt(counterStr) + 1;
-//
-//   $('#counter').text(counter.toString());
-// });
-//
-// //Second Cat
-//
-// //Add Cat Name
-// var secondcat = '<h2> Fluffy </h2>';
-// $('.secondcat').prepend(secondcat);
-//
-// //Count the clicks for the second cat
-// $('#secondcat').click(function(e) {
-//   var counterStr = $('#counter2').html();
-//   var counter = parseInt(counterStr) + 1;
-//
-//   $('#counter2').text(counter.toString());
-// });
