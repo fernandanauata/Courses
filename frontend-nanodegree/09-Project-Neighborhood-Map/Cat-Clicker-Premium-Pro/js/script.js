3dd1601edc444645e0b71775688a66294aa1552d
Fernanda Nauata <ferhcard@gmail.com>
2016-11-08 20:49:32 -0800

    Create the admin pro form, update readme

--- frontend-nanodegree/09-Project-Neighborhood-Map/Cat-Clicker-Premium-Pro/js/script.js
+++ frontend-nanodegree/09-Project-Neighborhood-Map/Cat-Clicker-Premium-Pro/js/script.js
@@ -40,69 +40,103 @@ $(function() {
 
     //Create the interation within the octupus
     var octopus = {
-      init: function(){
+        init: function() {
 
-        // Listener for menu
-        $( "li" ).on( "click", function(event) {
-          var newId = event.target.id.split("_")[0];
-          view.render(newId, model.currCat);
-          model.currCat = newId;
-      });
+            // Listener for menu
+            $("li").on("click", function(event) {
+                var newId = event.target.id.split("_")[0];
+                view.showCat(newId, model.currCat);
+                model.currCat = newId;
+            });
 
-        // Listener for images
-        $("img").on( "click", function(event) {
-          var id = event.target.id.split("_")[0];
-          octopus.updateCounter(id);
-        });
-    },
-     getAllCats: function(){
-       return model.cats;
-     },
-     updateCounter: function(id) {
-       model.cats.forEach(function(cat){
-         if (id === cat.id){
-           cat.counter++;
-           view.showCounter(id, cat.counter);
-         }
-       });
-     }
+            // Listener for images
+            $("img").on("click", function(event) {
+                var id = event.target.id.split("_")[0];
+                octopus.updateCounter(id);
+            });
+
+            //Listener for Admin button
+            $(".adminButton").on("click", function(event) {
+                $(".adminForm").show();
+            });
+
+            //Listener for Cancel button
+            $(".cancel").on("click", function(event) {
+                $(".adminForm").hide();
+            });
+
+            //Listener for Submit Button - changes currCat
+            $(".submit").on("click", function(event) {
+                model.cats.forEach(function(cat) {
+                    if (model.currCat === cat.id) {
+                        cat.title = $("#adminName").val();
+                        cat.src = $("#adminURL").val();
+                        cat.counter = parseInt($("#adminClicks").val());
+
+                        view.render(cat);
+                    }
+                });
+            });
+        },
+
+        getAllCats: function() {
+            return model.cats;
+        },
+
+        //Make the counter work
+        updateCounter: function(id) {
+            model.cats.forEach(function(cat) {
+                if (id === cat.id) {
+                    cat.counter++;
+                    view.showCounter(id, cat.counter);
+                }
+            });
+        }
     };
 
 
     //Render on the page
     var view = {
 
-      init: function() {
+        init: function() {
 
-        // Add list
-        octopus.getAllCats().forEach(function(cat) {
-          var HTMLlist = '<li id="%id%" class="#">%data%</li>';
-          $(".catlist").append(HTMLlist.replace("%id%", cat.id + "_item").replace("#", cat.id).replace("%data%", cat.title));
-        });
+            // Add list
+            octopus.getAllCats().forEach(function(cat) {
+                var HTMLlist = '<li id="%id%" class="#">%data%</li>';
+                $(".catlist").append(HTMLlist.replace("%id%", cat.id + "_item").replace("#", cat.id).replace("%data%", cat.title));
+            });
 
-        // Add cat containers
-        octopus.getAllCats().forEach(function(cat) {
-          var HTMLcontainer = '<nav id="#" class="catcontainer"></nav>';
-          var HTMLimg = '<img id="%id%" class="img-responsive" alt="#" src="%data%">';
+            // Add cat containers
+            octopus.getAllCats().forEach(function(cat) {
+                var HTMLcontainer = '<nav id="#" class="catcontainer"></nav>';
+                var HTMLimg = '<img id="%id%" class="img-responsive" alt="#" src="%data%">';
+                var HTMLcounter = '<h2 id=%h2% >%data%: <span id="%id%" class="counter">' + cat.counter.toString() + '</span></h2>';
+
+                $(".cat").append(HTMLcontainer.replace("#", cat.id));
+                $(".catcontainer:last-child").append(HTMLimg.replace("%id%", cat.id + "_img").replace("#", cat.alt).replace("%data%", cat.src));
+                $(".catcontainer:last-child").append(HTMLcounter.replace("%data%", cat.title).replace("%id%", cat.id + "_counter").replace("%h2%", cat.id + "_title"));
+            });
+            $(".catcontainer").hide();
+            $(".adminForm").hide();
+        },
+
+        showCat: function(newId, prevId) {
+            if (prevId != null) {
+                $("#" + prevId).hide();
+            }
+            $("#" + newId).show();
+        },
+        showCounter: function(id, count) {
+            $("#" + id + "_counter").text(count.toString());
+        },
+
+        //Add new cat created by the Admin form
+        render: function(cat) {
           var HTMLcounter = '<h2>%data%: <span id="%id%" class="counter">' + cat.counter.toString() + '</span></h2>';
 
-          $(".cat").append(HTMLcontainer.replace("#", cat.id));
-          $(".catcontainer:last-child").append(HTMLimg.replace("%id%", cat.id + "_img").replace("#", cat.alt).replace("%data%", cat.src));
-          $(".catcontainer:last-child").append(HTMLcounter.replace("%data%", cat.title).replace("%id%", cat.id + "_counter"));
-        });
-        $(".catcontainer").hide();
-      },
-
-      render: function(newId, prevId) {
-        if(prevId != null){
-          $("#" + prevId).hide();
-        }
-        $("#" + newId).show();
-      },
-      showCounter: function (id, count){
-        $("#" + id + "_counter" ).text(count.toString());
-      }
-
+          $("#" + cat.id + "_img").attr("src", cat.src);
+          $("#" + cat.id + "_title").replaceWith(HTMLcounter.replace("%data%", cat.title).replace("%id%", cat.id + "_counter"));
+        },
     };
 
     view.init();
--- frontend-nanodegree/09-Project-Neighborhood-Map/Cat-Clicker-Premium-Pro/readme.md
+++ frontend-nanodegree/09-Project-Neighborhood-Map/Cat-Clicker-Premium-Pro/readme.md
@@ -1,5 +1,5 @@
 # Cat Clicker Premium - Pro Version
 
-To use this application open this [Website](https://fernandanauata.github.io/Courses/frontend-nanodegree/09-Project-Neighborhood-Map/Cat-Clicker-Premium) and click on the name of any cat on the list. Every time you click on a cat image the counter adds 1, you can click on any from the list and their counter won't interfere each other.
+To use this application open this [Website](https://fernandanauata.github.io/Courses/frontend-nanodegree/09-Project-Neighborhood-Map/Cat-Clicker-Premium-Pro/) and click on the name of any cat on the list. Every time you click on a cat image the counter adds 1, you can click on any from the list and their counter won't interfere each other.
 
 The JavaScript is organized within a MVC (Model, View, Controller) architecture.
