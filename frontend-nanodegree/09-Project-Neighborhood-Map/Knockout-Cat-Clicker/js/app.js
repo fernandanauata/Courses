6345a8cdb0ef2a35ede7f7368457b7e0bee404db
Fernanda Nauata <ferhcard@gmail.com>
2016-11-11 16:30:05 -0800

    Cat Clicker Using Knockout JS

---
+++ frontend-nanodegree/09-Project-Neighborhood-Map/Knockout-Cat-Clicker/js/app.js
@@ -0,0 +1,12 @@
+var ViewModel = function() {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+  this.imgAttribution = ko.observable('http://www.fondox.net/wallpapers/un-gato-bebe-433.jpg');
+
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
+  };
+}
+
+ko.applyBindings(new ViewModel());
