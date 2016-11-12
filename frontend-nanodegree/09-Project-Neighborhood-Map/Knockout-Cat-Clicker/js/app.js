75b76ad60838786cec27df468c4b8da2ef28bc2b
Fernanda Nauata <ferhcard@gmail.com>
2016-11-12 12:21:44 -0800

    Added nicknames using knockout js  foreach

--- frontend-nanodegree/09-Project-Neighborhood-Map/Knockout-Cat-Clicker/js/app.js
+++ frontend-nanodegree/09-Project-Neighborhood-Map/Knockout-Cat-Clicker/js/app.js
@@ -1,12 +1,24 @@
 var ViewModel = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
-  this.imgAttribution = ko.observable('http://www.fondox.net/wallpapers/un-gato-bebe-433.jpg');
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+    this.imgAttribution = ko.observable('http://www.fondox.net/wallpapers/un-gato-bebe-433.jpg');
 
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-  };
-}
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+
+    this.nicknames = ko.observable(
+        [{
+            nickname: 'Tabtab'
+        }, {
+            nickname: 'T-Bone'
+        }, {
+            nickname: 'Mr. T'
+        }, {
+            nickname: 'Tabitha'
+        }]
+    )
+};
 
 ko.applyBindings(new ViewModel());
