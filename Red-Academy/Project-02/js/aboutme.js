adf6d00d387e800451c2d7c2fc265233fdf050a1
Fernanda Nauata <ferhcard@gmail.com>
2016-12-04 22:37:47 -0800

    Update

---
+++ Red-Academy/Project-02/js/aboutme.js
@@ -0,0 +1,16 @@
+$(function () { // wait for document ready
+    // Init
+    var controller = new ScrollMagic.Controller();
+
+    var tween = new TimelineMax ()
+    .add([
+      TweenMax.to(".aboutme-right-container", 1, {y: "-500vh"})
+    ]);
+
+    // build scene
+    var scene = new ScrollMagic.Scene({triggerElement: ".aboutme", duration: 2000, triggerHook: "onLeave"})
+            .setTween(tween)
+            .setPin(".aboutme")
+            // .addIndicators() // add indicators (requires plugin)
+            .addTo(controller);
+  });
--- Red-Academy/Project-02/js/script.js
+++ Red-Academy/Project-02/js/script.js
@@ -16,7 +16,26 @@ $(function() {
     //Smal Portfolio Carousel
     $('.carousel').slick({
         autoplay: true,
-        arrows: false
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }]
     });
 
 
