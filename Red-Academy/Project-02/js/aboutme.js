28c248dddf51334755458983f947cda37840e1c5
Fernanda Nauata <ferhcard@gmail.com>
2016-12-06 19:28:34 -0800

    Test About Me bug

--- Red-Academy/Project-02/js/aboutme.js
+++ Red-Academy/Project-02/js/aboutme.js
@@ -1,16 +1,25 @@
-$(function () { // wait for document ready
-    // Init
-    var controller = new ScrollMagic.Controller();
+$(function() { // wait for document ready
+    //Detect if it is a phone
+    if (screen && screen.width > 599) {
+        // Init
+        var controller = new ScrollMagic.Controller();
 
-    var tween = new TimelineMax ()
-    .add([
-      TweenMax.to(".aboutme-right-container", 1, {y: "-500vh"})
-    ]);
+        var tween = new TimelineMax()
+            .add([
+                TweenMax.to(".aboutme-right-container", 1, {
+                    y: "-500vh"
+                })
+            ]);
 
-    // build scene
-    var scene = new ScrollMagic.Scene({triggerElement: ".aboutme", duration: 2000, triggerHook: "onLeave"})
+        // build scene
+        var scene = new ScrollMagic.Scene({
+                triggerElement: ".aboutme",
+                duration: 2000,
+                triggerHook: "onLeave"
+            })
             .setTween(tween)
             .setPin(".aboutme")
             .addIndicators() // add indicators (requires plugin)
             .addTo(controller);
-  });
+    }
+});
